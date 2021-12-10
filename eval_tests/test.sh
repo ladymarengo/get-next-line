c6a69a0580f0e9e7dc35d949035280b896a5f725
Natalia Samoilova <nsamoilo@c2r6p5.hive.fi>
2021-12-10 12:51:37 +0200

    Update tests

--- eval_tests/test.sh
+++ eval_tests/test.sh
@@ -1,7 +1,15 @@
 mv main.c.test main.c
-make -C ../libft/
 gcc -Wall -Wextra -Werror main.c ../get_next_line.c ../libft/libft.a -o get_next_line
-./get_next_line test
+./get_next_line testfiles/test_bible.txt
+./get_next_line testfiles/empty
+./get_next_line testfiles/one_newline
+./get_next_line testfiles/two_newlines
+./get_next_line testfiles/random_test
+./get_next_line testfiles/4char_1line
+./get_next_line testfiles/4char_2line
+./get_next_line testfiles/4char_5line
+./get_next_line testfiles/16char_1line
+./get_next_line testfiles/16char_2line
+./get_next_line
 rm get_next_line
-make -C ../libft/ fclean
 mv main.c main.c.test
